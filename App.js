0b14e1fd7ea15d3cc4bcfd6ece0d024a43011eb0
Xyza Rivera <xyzarivera@gmail.com>
2021-11-20 12:44:47 +0800

    remove unused code

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
-import {LoadingScren, LoginForm, SignupForm} from './screens';
+import {LoginForm, SignupForm} from './screens';
 import Menu from './navigation/Menu';
 import {AuthContext} from './context';
 import LoadingScreen from './screens/LoadingScreen';
@@ -27,20 +13,11 @@ import LoadingScreen from './screens/LoadingScreen';
 const AuthStack = createNativeStackNavigator();
 const Stack = createNativeStackNavigator();
 
-const App: () => Node = () => {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
     backgroundColor: isDarkMode ? '#333' : '#ccc',
   };
-
-  //   <SafeAreaView style={backgroundStyle}>
-  //   <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-  //   <ScrollView
-  //     contentInsetAdjustmentBehavior="automatic"
-  //     style={backgroundStyle}>
-
-  //   </ScrollView>
-  // </SafeAreaView>
 
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -82,25 +59,5 @@ const App: () => Node = () => {
     );
   }
 };
-
-//Styles Example
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
