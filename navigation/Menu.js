e590d90ef6dfc4cbe395c16cdf7f638568642df6
Gabriel Goodman <gabrielgoodman@Gabriels-MacBook-Pro.local>
2021-11-20 14:56:10 +0900

    Shinjuku Station location page template WIP

--- navigation/Menu.js
+++ navigation/Menu.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { searchContext } from "../components/searchContext"
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {searchContext} from '../components/searchContext';
 
-import HomeScreen from "../screens/HomeScreen";
+import HomeScreen from '../screens/HomeScreen';
 //import ResultsScreen from "../screens/ResultsScreen";
 import SearchStackScreen from '../screens/SearchStackScreen';
 import SavedScreen from '../screens/SavedScreen';
@@ -11,17 +11,17 @@ import ProfileScreen from '../screens/ProfileScreen';
 const Tab = createBottomTabNavigator();
 
 const Menu = ({queryString, setQueryString}) => {
-  console.log('query',queryString);
-  return(
+  console.log('query', queryString);
+  return (
     <searchContext.Provider value={[queryString, setQueryString]}>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Search" component={SearchStackScreen} />
-      <Tab.Screen name="Saved" component={SavedScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-    </Tab.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Search" component={SearchStackScreen} />
+        <Tab.Screen name="Saved" component={SavedScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
     </searchContext.Provider>
-  )
-}
+  );
+};
 
 export default Menu;
