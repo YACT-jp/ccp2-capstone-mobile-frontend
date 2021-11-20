8418c067992844ea90d921dcd5e7a3be2484b6e2
Xyza Rivera <xyzarivera@gmail.com>
2021-11-20 12:41:29 +0800

    create loading screen

---
+++ screens/LoadingScreen.js
@@ -0,0 +1,39 @@
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, Image, Button} from 'react-native';
+import appLogo from '../assets/logo-app.png';
+
+const LoadingScreen = () => {
+  useEffect(() => {
+    // load initial data (location, anime) to local storage
+  }, []);
+
+  const testButton = () => {
+    console.log('test');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={appLogo} style={styles.logo} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: '80%',
+    resizeMode: 'contain',
+  },
+  buttonsContainer: {
+    padding: 10
+  },
+  buttonsStyle: {
+    margin: 10
+  },
+});
+
+export default LoadingScreen;
