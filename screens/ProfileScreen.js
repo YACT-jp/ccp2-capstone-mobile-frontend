90adafae819d89dc0dd00887abc234c4590f6f75
Gabe-Goodman <goodman.g@husky.neu.edu>
2021-11-25 20:24:52 +0900

    Wrapped components in SafeAreaView tag

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet, useColorScheme} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  useColorScheme,
+  SafeAreaView,
+} from 'react-native';
 import {useAuth, AuthProvider} from '../providers/AuthProvider';
 
 function ProfileScreen({navigation}) {
   const {user, signUp, signOut} = useAuth();
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View
+    <SafeAreaView
       style={{
         backgroundColor: isDarkMode ? '#000' : '#fff',
       }}>
-      <Text>Your profile here</Text>
-      {/* <Button
+      <View
+        style={{
+          backgroundColor: isDarkMode ? '#000' : '#fff',
+        }}>
+        <Text>Your profile here</Text>
+        {/* <Button
         title="Go to Results"
         onPress={() => navigation.navigate('Search')}
       /> */}
-      <Button
-        title="Sign Out"
-        onPress={() => {
-          signOut();
-          // navigation.navigate('Login');
-        }}
-      />
-    </View>
+        <Button
+          title="Sign Out"
+          onPress={() => {
+            signOut();
+            // navigation.navigate('Login');
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
--- screens/SavedScreen.js
+++ screens/SavedScreen.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet, useColorScheme } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  useColorScheme,
+  SafeAreaView,
+} from 'react-native';
 
 function SavedScreen({navigation}) {
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View
-    style={{
-      backgroundColor: isDarkMode ? '#000' : '#fff',
-    }}>
-      <Text>Saved Location go here</Text>
-  </View>
+    <SafeAreaView
+      style={{
+        backgroundColor: isDarkMode ? '#000' : '#fff',
+      }}>
+      <View
+        style={{
+          backgroundColor: isDarkMode ? '#000' : '#fff',
+        }}>
+        <Text>Saved Location go here</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
