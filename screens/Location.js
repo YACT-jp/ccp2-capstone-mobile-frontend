8a835c43b1b5c10e2eedda397dcd03fa213533b7
Xyza Rivera <xyzarivera@gmail.com>
2021-11-27 11:09:12 +0800

    improve UI and Add Save Location function

--- screens/Location.js
+++ screens/Location.js
@@ -1,105 +1,149 @@
 import React from 'react';
-import {View, Button, StyleSheet, useColorScheme} from 'react-native';
 import {
   Heading,
   Text,
   VStack,
-  ZStack,
   Box,
   Image,
   Center,
   NativeBaseProvider,
-  useColorModeValue,
+  AspectRatio,
+  Stack,
+  ScrollView,
+  Button,
 } from 'native-base';
-// import MapView from 'react-native-maps';
+import {useAuth} from '../providers/AuthProvider';
 
 function Location({route, navigation}) {
   /*Get the params */
-  const {fullItem, cameFrom} = route.params;
-  const isDarkMode = useColorScheme() === 'dark';
-  console.log(fullItem);
-  console.log(cameFrom);
+  const {fullItem} = route.params;
+  const {location_pic} = fullItem;
+  // console.log(fullItem);
+  const {user, signUp, signIn} = useAuth();
+
+  const bookmarkEndpoint = async (inputdata) => {
+    console.log('input', inputdata);
+    try {
+      const response = await fetch(
+        `https://ccp2-capstone-backend-sa-yxiyypij7a-an.a.run.app/api/user/${user.id}/bookmarks`,
+        {
+          method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
+          // mode: 'cors', // no-cors, *cors, same-origin
+          // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          // credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          // redirect: 'follow', // manual, *follow, error
+          // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          body: JSON.stringify(inputdata), // body data type must match "Content-Type" header
+        },
+      );
+      // console.log(response);
+      const data = await response.text();
+      return data;
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
+
+  const onSaveClick = () => {
+    console.log('click');
+    console.log('user', user.id);
+    fetchData = async () => {
+      const data = await bookmarkEndpoint(fullItem);
+      console.log('bookmark data', data);
+    };
+    fetchData();
+  };
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const data = await mediaResultsApi();
+  //     setDATA(data);
+  //   }
+  //   fetchData();
+  // }, []);
+
   return (
-    //   <View
-    //   style={{
-    //     backgroundColor: isDarkMode ? '#000' : '#fff',
-    //   }}>
-    //     <Text>Detailed Location Info</Text>
-    //     <Text>{fullItem.name}</Text>
-    //     <Text>{fullItem._id}</Text>
-    //     <Text>{fullItem.plus_code}</Text>
-    //     <Button title="Go back" onPress={() => navigation.goBack()} />
-    // </View>
     <NativeBaseProvider>
-      <Center flex={1} px="3">
-        <View
-          style={{
-            backgroundColor: isDarkMode ? '#000' : '#fff',
-            flex: 1,
-            padding: 20,
-          }}>
-          <Heading textAlign="center">{fullItem.name}</Heading>
-          <Center flex={1} px="3">
-            <Image
-              size={150}
-              resizeMode={'contain'}
-              borderRadius={100}
-              source={{
-                uri: fullItem.location_pic,
-              }}
-              alt="Alternate Text"
-            />
-          </Center>
-          <View style={{flex: 4}}>
-            <Box
-              width={72}
-              bg={useColorModeValue('gray.50', 'gray.700')}
-              shadow={1}>
-              <VStack
-                space={1}
-                alignItems="center"
-                justifyItems="center"
-                mt={3}>
-                {/* <Heading textAlign="center">{fullItem.name}</Heading> */}
-                {/* <Text>{fullItem.description}</Text> */}
-                <Text fontSize="lg">Details:</Text>
-                <Text textAlign="center">{fullItem.coordinates}</Text>
-                <Text textAlign="center" padding="3">
-                  {fullItem.description}
-                </Text>
-                <Text>{fullItem['Plus Code']}</Text>
-                <Button title="Go back" onPress={() => navigation.goBack()} />
-              </VStack>
-            </Box>
-          </View>
-          {/* <MapView
-            initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          /> */}
-        </View>
+      <Center flex={1}>
+        <Box flex="1" safeAreaTop>
+          <ScrollView>
+            <VStack space={4} alignItems="center">
+              <AspectRatio w="100%" ratio={16 / 9}>
+                {location_pic === '' || location_pic === null ? (
+                  <Center flex="1">
+                    <Text fontWeight="400">No Image yet</Text>
+                  </Center>
+                ) : (
+                  <Image
+                    source={{
+                      uri: fullItem.location_pic,
+                    }}
+                    alt="image"
+                  />
+                )}
+                {/* <Image
+                  source={{
+                    uri: fullItem.location_pic,
+                  }}
+                  alt="image"
+                /> */}
+              </AspectRatio>
+              <Box
+                safeArea
+                w="80"
+                maxW="80"
+                rounded="lg"
+                overflow="hidden"
+                borderColor="coolGray.200"
+                borderWidth="1"
+                p="2"
+                py="8"
+                _dark={{
+                  borderColor: 'coolGray.600',
+                  backgroundColor: 'gray.700',
+                }}
+                _web={{
+                  shadow: 2,
+                  borderWidth: 0,
+                }}
+                _light={{
+                  backgroundColor: 'gray.50',
+                }}>
+                <Stack p="4" space={3}>
+                  <Stack space={2}>
+                    <Heading size="md" ml="-1">
+                      {fullItem.name}
+                    </Heading>
+                    <Text
+                      fontSize="xs"
+                      _light={{
+                        color: 'violet.500',
+                      }}
+                      _dark={{
+                        color: 'violet.400',
+                      }}
+                      fontWeight="500"
+                      ml="-0.5"
+                      mt="-1">
+                      {`Media Name`}
+                    </Text>
+                  </Stack>
+                  <Text fontWeight="400">{fullItem.description}</Text>
+                  <Button size="sm" onPress={onSaveClick}>
+                    Save Location
+                  </Button>
+                </Stack>
+              </Box>
+            </VStack>
+          </ScrollView>
+        </Box>
       </Center>
     </NativeBaseProvider>
   );
 }
 
 export default Location;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     //marginTop: StatusBar.currentHeight || 0,
-//   },
-//   item: {
-//     backgroundColor: '#f9c2ff',
-//     padding: 20,
-//     marginVertical: 8,
-//     marginHorizontal: 16,
-//   },
-//   name: {
-//     fontSize: 32,
-//   },
-// });
