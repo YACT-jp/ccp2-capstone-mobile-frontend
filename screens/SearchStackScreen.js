09c5f6c241a4e27b91d0920649e09abdfdbb8585
Gabe-Goodman <goodman.g@husky.neu.edu>
2021-12-02 21:04:40 +0900

    Rendered button in header

--- screens/SearchStackScreen.js
+++ screens/SearchStackScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+import {Button, SearchIcon, IconButton} from 'native-base';
 import ResultsScreen from '../screens/ResultsScreen';
 import Location from './Location';
 import LocResultsScreen from './LocResultsScreen';
@@ -12,7 +12,21 @@ const SearchStack = createNativeStackNavigator();
 function SearchStackScreen() {
   return (
     <SearchStack.Navigator>
-      <SearchStack.Screen name="Media Results" component={ResultsScreen} />
+      <SearchStack.Screen
+        name="Media Results"
+        component={ResultsScreen}
+        options={{
+          headerRight: () => (
+            <IconButton
+              size="5"
+              mt="0.5"
+              onPress={() => navigation.goBack()}
+              title="Info"
+              color="#fff"
+            />
+          ),
+        }}
+      />
       <SearchStack.Screen name="Locations" component={LocResultsScreen} />
       <SearchStack.Screen name="Location" component={Location} />
     </SearchStack.Navigator>
