17b39115c92966b0422f156c9f8b17770316457d
Gabe-Goodman <goodman.g@husky.neu.edu>
2021-12-04 13:54:02 +0900

    Implemented UI changes

--- screens/SavedScreen.js
+++ screens/SavedScreen.js
@@ -1,14 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {
-  View,
-  Text,
   StyleSheet,
   useColorScheme,
   FlatList,
   TouchableOpacity,
-  Button,
   SafeAreaView,
 } from 'react-native';
+import {
+  Button,
+  HStack,
+  View,
+  Pressable,
+  Text,
+  Image,
+  NativeBaseProvider,
+} from 'native-base';
 
 import {useAuth} from '../providers/AuthProvider';
 import {getAsyncSavedLocations} from '../data/asyncSavedLocations';
@@ -19,12 +25,12 @@ function SavedScreen({navigation}) {
   const [DATA, setDATA] = useState([]);
   const [refresh, setRefresh] = useState(false);
 
-  // load Saved Locations from AsyncStorage 
+  // load Saved Locations from AsyncStorage
   useEffect(() => {
     async function fetchData() {
       try {
         const res = await getAsyncSavedLocations();
-        setDATA(res)
+        setDATA(res);
         return res;
       } catch (error) {
         throw error;
@@ -35,17 +41,61 @@ function SavedScreen({navigation}) {
 
   //List Item Component
   const Item = ({name, fullItem}) => (
-    <View style={styles.item}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Saved Location', {fullItem})}>
-        <Text style={styles.name}>{name}</Text>
-        <Text>{fullItem['plus_code']}</Text>
-      </TouchableOpacity>
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.item} rounded="xl">
+        <Pressable
+          rounded="xl"
+          onPress={() => navigation.navigate('Saved Location', {fullItem})}>
+          <HStack
+            style={styles.container}
+            space={5}
+            justifyContent="space-between">
+            <Image
+              border={1}
+              borderWidth={5}
+              borderColor="white"
+              height={150}
+              borderRadius={150}
+              source={{
+                uri: fullItem.location_pic,
+              }}
+              alt="Alternate Text"
+              size="xl"
+              ml="5"
+            />
+            <Text
+              fontSize="4xl"
+              lineHeight="sm"
+              color="white"
+              isTruncated
+              maxW="180"
+              w="80%"
+              multiline={true}
+              numberOfLines={3}>
+              {name}
+            </Text>
+          </HStack>
+        </Pressable>
+      </View>
+    </NativeBaseProvider>
   );
 
   //Process each item of the data array
   const renderItem = ({item}) => <Item name={item.name} fullItem={item} />;
+
+  const _renderFooter = () => (
+    <View style={{paddingVertical: 4}}>
+      <Button
+        mt="1"
+        margin="2"
+        colorScheme="blue"
+        onPress={() =>
+          navigation.navigate('Search', {screen: 'Media Results'})
+        }>
+        Back to results
+      </Button>
+    </View>
+  );
 
   return (
     <SafeAreaView
@@ -55,19 +105,19 @@ function SavedScreen({navigation}) {
       <View
         style={{
           backgroundColor: isDarkMode ? '#000' : '#fff',
-          paddingBottom: 230,
+          paddingBottom: 90,
         }}>
-        <Text style={styles.name}>Saved Locations</Text>
-        <Button title="Refresh" onPress={() => setRefresh(!refresh)} />
-        <FlatList data={DATA} renderItem={renderItem}></FlatList>
-
-        {/* Need to pass mediaId to Locations page */}
         <Button
-          title="Back to search"
-          onPress={() =>
-            navigation.navigate('Search', {screen: 'Media Results'})
-          }
-        />
+          margin="2"
+          mb="1"
+          colorScheme="blue"
+          onPress={() => setRefresh(!refresh)}>
+          Refresh
+        </Button>
+        <FlatList
+          ListFooterComponent={() => _renderFooter()}
+          data={DATA}
+          renderItem={renderItem}></FlatList>
       </View>
     </SafeAreaView>
   );
@@ -77,16 +127,14 @@ export default SavedScreen;
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 1,
-    //marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    backgroundColor: '#eeee33',
+    backgroundColor: '#3b81f6',
     padding: 20,
-    marginVertical: 8,
+    marginVertical: 2,
     marginHorizontal: 16,
-  },
-  name: {
-    fontSize: 32,
   },
 });
