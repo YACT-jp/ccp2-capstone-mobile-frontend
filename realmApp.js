781cd068fe252ae3e93a86035b90ef630f2f8417
Xyza Rivera <xyzarivera@gmail.com>
2021-12-09 20:27:59 +0800

    remove app ID with fresh file

---
+++ realmApp.js
@@ -0,0 +1,6 @@
+import Realm from 'realm';
+import {APPID} from "@env"
+
+// Invokes the shared instance of the Realm app.
+const app = new Realm.App({id: APPID}); // Set Realm app ID here.
+export default app;
