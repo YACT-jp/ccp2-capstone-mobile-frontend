fb553450956ac8522e3e3dfe91f5e0a53d6cfe90
Xyza Rivera <xyzarivera@gmail.com>
2021-12-12 19:07:31 +0800

    fix merge conflicts

--- screens/profile/Profile.js
+++ screens/profile/Profile.js
@@ -16,8 +16,9 @@ import {
 } from 'native-base';
 import {useFocusEffect} from '@react-navigation/core';
 import {useAuth} from '../../providers/AuthProvider';
-import {photosByUser, getProfile} from '../../data/data';
+import {photosByUser, getProfile, deletePhoto} from '../../data/data';
 import {retrieveUserSession} from '../../data/secureStorage';
+import ProfileGalleryNav from '../ProfileGalleryNav';
 
 function ProfileScreen({navigation}) {
   //Find additional functions on the user object
@@ -36,14 +37,14 @@ function ProfileScreen({navigation}) {
     const userData = await retrieveUserSession();
     console.log('SECURE STORAGE:', userData);
     const nowTime = new Date();
-    if(!userData) console.log('ERROR-No userData');
-    if(!userData["timestamp"]) console.log('ERROR-No timestamp in userData');
-    if(userData && userData["timestamp"]) {
-      const thenTime = new Date(userData["timestamp"]);
+    if (!userData) console.log('ERROR-No userData');
+    if (!userData['timestamp']) console.log('ERROR-No timestamp in userData');
+    if (userData && userData['timestamp']) {
+      const thenTime = new Date(userData['timestamp']);
       const maxDiff = 86400000 * 0.5; //Days in milliseconds * number of days to refresh token
       if (nowTime.getTime() - thenTime.getTime() > maxDiff) {
-        console.log ('====== NEED TO REFRESH TOKEN AFTER HALF DAY ======');
-      }   
+        console.log('====== NEED TO REFRESH TOKEN AFTER HALF DAY ======');
+      }
     }
   };
   const {username, email, bio} = userInfo;
@@ -54,6 +55,7 @@ function ProfileScreen({navigation}) {
   const [singlePhoto, setSinglePhoto] = useState(); // passes only URL
   const [currentIndex, setCurrentIndex] = useState();
   const [deleteId, setDeleteId] = useState(); // passes photo object id to delete
+  const [refresh, setRefresh] = useState(false);
 
   useFocusEffect(
     useCallback(() => {
@@ -67,7 +69,7 @@ function ProfileScreen({navigation}) {
       return () => {
         console.log('unmount my profile');
       };
-    }, []),
+    }, [refresh]),
   );
 
   /** update photo url for singlePhoto modal */
@@ -79,62 +81,17 @@ function ProfileScreen({navigation}) {
     event.preventDefault();
   };
 
-  const lastPhoto = (event, item) => {
-    if (currentIndex === 0) {
-      setCurrentIndex(DATA.length - 1);
-      setSinglePhoto(DATA[`${currentIndex}`]['url']);
-    } else {
-      setCurrentIndex(currentIndex - 1);
-    }
-    console.log('singlePhoto', singlePhoto);
-    console.log('currentIndex', currentIndex);
-    event.preventDefault();
-  };
-
-  const nextPhoto = (event, item) => {
-    if (currentIndex === DATA.length - 1) {
-      setCurrentIndex(0);
-      setSinglePhoto(DATA[0]['url']);
-    } else {
-      setCurrentIndex(currentIndex + 1);
-    }
-    console.log('singlePhoto', singlePhoto);
-    console.log('currentIndex', currentIndex);
-    event.preventDefault();
-  };
-
   //Process each item of the data array
   const renderItem = ({item}) => <Item url={item.url} item={item} />;
-
-  /** DELETE request sending imageUri to backend */
-  const deleteImage = (imageUri, photoDescription) => {
-    const url = `https://ccp2-capstone-backend-sa-yxiyypij7a-an.a.run.app/api/photo/${currentPhoto._id}`;
-    let formData = new FormData();
-    formData.append('file', {
-      uri: imageUri,
-      name: 'image.jpg',
-      type: 'image/jpeg',
-    });
-    formData.append('description', photoDescription);
-    return fetch(url, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      body: formData,
-    }).catch(error => {
-      console.warn(error);
-    });
-  };
 
   // Header element for the scrolling Flatlist
   const _renderHeader = () => (
     <Stack p="8" space={3}>
       <Stack space={2}>
         <Heading size="md" ml="-1">
-        {profileData.username === null || profileData.username === undefined
-          ? username
-          : profileData.username}
+          {profileData.username === null || profileData.username === undefined
+            ? username
+            : profileData.username}
         </Heading>
       </Stack>
       <Text fontWeight="400">
@@ -183,62 +140,6 @@ function ProfileScreen({navigation}) {
     </Pressable>
   );
 
-  const SinglePhoto = (item, DATA) => (
-    <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-      <Modal.Content size="lg">
-        <Modal.CloseButton />
-        <Modal.Header>Your Image Gallery</Modal.Header>
-        <Modal.Body space={5} alignItems="center">
-          <HStack space={5} alignItems="center" justifyContent="center">
-            <ArrowBackIcon onPress={(event, item) => lastPhoto(event, item)} />
-            <Image
-              border={1}
-              borderWidth={5}
-              borderColor="white"
-              source={{
-                uri: singlePhoto,
-              }}
-              alt="Alternate Text"
-              size="2xl"
-            />
-            <ArrowForwardIcon
-              onPress={(event, item) => nextPhoto(event, item)}
-            />
-          </HStack>
-          {singleDescription ? (
-            <Text>{singleDescription}</Text>
-          ) : (
-            <Text>No description</Text>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button.Group space={2}>
-            <Button
-              variant="ghost"
-              colorScheme="blueGray"
-              onPress={() => {
-                setShowModal(false);
-              }}>
-              Back
-            </Button>
-            <Button
-              colorScheme="blue"
-              onPress={() => {
-                setShowModal(false);
-                console.log(currentPhoto._id);
-                // postImage(imageUri),
-                // setTimeout(() => {
-                //   setGalleryRefresh(!galleryRefresh);
-                // }, 1000);
-              }}>
-              Delete
-            </Button>
-          </Button.Group>
-        </Modal.Footer>
-      </Modal.Content>
-    </Modal>
-  );
-
   return (
     <NativeBaseProvider>
       <Center flex={1}>
@@ -249,7 +150,45 @@ function ProfileScreen({navigation}) {
           data={DATA}
           renderItem={renderItem}
           style={{paddingHorizontal: 4, width: '100%'}}></FlatList>
-        <SinglePhoto />
+        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+          <Modal.Content size="lg">
+            <Modal.CloseButton />
+            <Modal.Header>Your Image Gallery</Modal.Header>
+            <Modal.Body space={5} alignItems="center">
+              <ProfileGalleryNav
+                DATA={DATA}
+                item={singlePhoto}
+                showModalInit={showModal}
+                singlePhoto={singlePhoto}
+                currentIndex={currentIndex}
+                deleteId={deleteId}
+                setDeleteId={setDeleteId}
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button.Group space={2}>
+                <Button
+                  variant="ghost"
+                  colorScheme="blueGray"
+                  onPress={() => {
+                    setShowModal(false);
+                  }}>
+                  Back
+                </Button>
+                <Button
+                  colorScheme="blue"
+                  onPress={async () => {
+                    setShowModal(false);
+                    let results = await deletePhoto(deleteId);
+                    console.log(results);
+                    setRefresh(!refresh);
+                  }}>
+                  Delete
+                </Button>
+              </Button.Group>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
       </Center>
     </NativeBaseProvider>
   );
