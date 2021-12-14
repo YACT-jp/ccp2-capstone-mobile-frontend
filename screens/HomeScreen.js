9393df14aa6afe6191c31efb5f3352eb2bf3209c
Norifumi Kimura <norical496@gmail.com>
2021-12-14 20:23:24 +0900

    Shows three random media after login

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import {
-  View,
-  Text,
   TextInput,
   StyleSheet,
   useColorScheme,
   SafeAreaView,
+  FlatList
 } from 'react-native';
-import {Button} from 'native-base';
-import {searchContext} from '../providers/SearchProvider';
+import {Button, Text, HStack, View, VStack, Pressable, Image, NativeBaseProvider} from 'native-base';
+import { searchContext } from '../providers/SearchProvider';
 import { mediaResultsApi } from '../data/data';
 
 function HomeScreen({navigation}) {
@@ -18,25 +17,103 @@ function HomeScreen({navigation}) {
   const [queryString, setQueryString] = React.useContext(searchContext);
   const [threeRandomMedia, setThreeRandomMedia] = useState([]);
 
-
+  //Hook
   useEffect(async () => {
-    async function fetchThreeRandomData() {
-      const data = await mediaResultsApi();
-
-      const randomThree = [];
-      while(randomThree.length < 3){
-        let randomIndex = Math.floor(Math.random() * data.length);
-        
-        if(!randomThree.includes(randomIndex)){
-          randomThree.push(randomIndex);
-        }
-      }
-      return randomThree.map(index => data[index]);
-    }
     const fetchedThreeMedia = await fetchThreeRandomData();
-    console.log(fetchedThreeMedia);
     setThreeRandomMedia(fetchedThreeMedia);
   },[]);
+
+  //Hanlder
+  async function fetchThreeRandomData() {
+    const data = await mediaResultsApi();
+
+    const randomThree = [];
+    while(randomThree.length < 3){
+      let randomIndex = Math.floor(Math.random() * data.length);
+      
+      if(!randomThree.includes(randomIndex)){
+        randomThree.push(randomIndex);
+      }
+    }
+    return randomThree.map(index => data[index]);
+  }
+
+  const renderItem = ({item}) =>
+  item.name.toLowerCase().includes(queryString.toLowerCase()) ? (
+    <Item
+      name={item.name}
+      path={item['poster_path']}
+      description={item['overview']}
+      mediaId={item['id']}
+    />
+  ) : null;
+
+  //Rendering function
+  const Item = ({name, path, description, mediaId}) => (
+    <NativeBaseProvider>
+      <View style={styles.item} rounded="lg">
+        <Pressable
+          rounded="lg"
+          onPress={() =>
+            navigation.navigate('Location Results', {name, mediaId})
+          }>
+          <HStack style={styles.container} space={5}>
+            <Image
+              border={1}
+              borderWidth={2}
+              borderColor="white"
+              height={150}
+              borderRadius={150}
+              source={{
+                uri: 'https://image.tmdb.org/t/p/w500' + path,
+              }}
+              alt={`${name} poster`}
+              size="md"
+              ml="2"
+            />
+            <VStack>
+              <Text
+                fontSize="xl"
+                color="white"
+                isTruncated
+                maxW="225"
+                lineHeight="xs">
+                {name}
+              </Text>
+              <Text
+                mt="1"
+                fontSize="2xs"
+                color="white"
+                isTruncated
+                maxW="225"
+                fontWeight="500"
+                multiline={true}
+                numberOfLines={3}>
+                {description}
+              </Text>
+            </VStack>
+          </HStack>
+        </Pressable>
+      </View>
+    </NativeBaseProvider>
+  );
+
+  const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+    },
+    item: {
+      backgroundColor: '#3b81f6',
+      padding: 10,
+      marginVertical: 2,
+      marginHorizontal: 16,
+    },
+    name: {
+      fontSize: 32,
+    },
+  });
 
   return (
     <SafeAreaView
@@ -44,6 +121,7 @@ function HomeScreen({navigation}) {
         backgroundColor: isDarkMode ? '#000' : '#fff',
       }}>
       <View
+        style={styles.container}
         style={{
           backgroundColor: isDarkMode ? '#000' : '#fff',
         }}>
@@ -60,6 +138,7 @@ function HomeScreen({navigation}) {
         <Text style={{fontSize: 24}}>
           {'\n'}You entered: {text}
         </Text>
+        <FlatList data={threeRandomMedia} renderItem={renderItem}></FlatList>
         <Button
           margin="2"
           colorScheme="blue"
@@ -75,13 +154,4 @@ function HomeScreen({navigation}) {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'lightgray',
-  },
-  text: {
-    color: 'rgb(59,108,212)',
-  },
-});
+
